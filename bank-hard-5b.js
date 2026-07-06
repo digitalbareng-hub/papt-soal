54568f6b4d0cc8013612fa7225d04e58d9bf83a5
digitalbareng-hub <digitalbareng@gmail.com>
2026-07-06 17:58:03 +0700

    Tambah soal sulit bagian 5b

---
+++ bank-hard-5b.js
@@ -0,0 +1,3 @@
+export const HARD_5B = [
+['SULIT','Pembinaan Pengawasan PPAT','Pengawas menemukan daftar akta tidak ditutup harian dan laporan bulanan terlambat. Fokus penilaian tepat adalah ...',['Kepatuhan pelaksanaan jabatan, pencatatan akta, laporan bulanan, dan protokol PPAT.','Estetika ruang tunggu kantor saja.','Merek komputer kantor.','Selera desain papan nama.','Promosi media sosial tanpa kaitan jabatan.'],'Pembinaan dan pengawasan PPAT menilai pelaksanaan jabatan dan kepatuhan administrasi/protokol.']
+];
