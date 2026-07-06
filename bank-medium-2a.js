5f7a53fb1c4c5cf52aef05af9610c1659c99912a
digitalbareng-hub <digitalbareng@gmail.com>
2026-07-06 17:59:21 +0700

    Tambah soal mudah bagian 2a

---
+++ bank-medium-2a.js
@@ -0,0 +1,5 @@
+export const MEDIUM_2A = [
+['MUDAH','Hukum Pertanahan Nasional','Pasal UUPA yang menjadi dasar pendaftaran tanah adalah ...',['Pasal 19.','Pasal 2.','Pasal 20.','Pasal 33.','Pasal 37.'],'Pasal 19 UUPA memerintahkan pendaftaran tanah.'],
+['MUDAH','Pendaftaran Tanah','Objek pendaftaran tanah meliputi ...',['Hak atas tanah, HPL, tanah wakaf, HMSRS, Hak Tanggungan, dan tanah negara.','Hanya Hak Milik.','Hanya tanah sengketa.','Hanya tanah bank.','Hanya bangunan tanpa tanah.'],'PP 24/1997 memuat objek pendaftaran tanah yang luas, termasuk tanah negara dan Hak Tanggungan.'],
+['MUDAH','Pendaftaran Tanah','Satuan wilayah tata usaha pendaftaran tanah pada umumnya adalah ...',['Desa atau kelurahan.','Provinsi.','Nasional.','RT saja.','Blok pajak.'],'Pada umumnya satuan wilayah tata usaha pendaftaran tanah adalah desa atau kelurahan.']
+];
