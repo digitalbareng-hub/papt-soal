55029c8e5bad6b8fca18c79e59089d5d91afcb76
digitalbareng-hub <digitalbareng@gmail.com>
2026-07-06 17:59:58 +0700

    Tambah soal mudah bagian 2b

---
+++ bank-medium-2b.js
@@ -0,0 +1,3 @@
+export const MEDIUM_2B = [
+['MUDAH','Pendaftaran Tanah','Data fisik tanah berkaitan dengan ...',['Letak, batas, luas, dan keterangan fisik objek.','Honorarium PPAT.','Kode etik.','Magang IPPAT.','Struktur kementerian.'],'Data fisik menjelaskan keadaan fisik objek tanah.']
+];
