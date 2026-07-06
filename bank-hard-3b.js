72841d8a6f128496cfb3741c4b08a8672f97f221
digitalbareng-hub <digitalbareng@gmail.com>
2026-07-06 17:55:03 +0700

    Tambah soal sulit bagian 3b

---
+++ bank-hard-3b.js
@@ -0,0 +1,3 @@
+export const HARD_3B = [
+['SULIT','Peraturan Jabatan PPAT','PPAT membuka ruang layanan terpisah sebagai perwakilan kantor. Jawaban paling tepat adalah ...',['Tidak diperbolehkan karena PPAT tidak boleh membuka kantor cabang atau perwakilan.','Diperbolehkan jika masih satu provinsi.','Diperbolehkan jika tanpa papan nama.','Diperbolehkan jika ramai pemohon.','Diperbolehkan jika disetujui saksi.'],'Perkaban melarang kantor cabang atau perwakilan untuk menawarkan jasa PPAT.']
+];
