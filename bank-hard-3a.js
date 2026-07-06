4c3fd8e62df8decc69a353a8b11578e5ee44e651
digitalbareng-hub <digitalbareng@gmail.com>
2026-07-06 17:54:41 +0700

    Tambah soal sulit bagian 3a

---
+++ bank-hard-3a.js
@@ -0,0 +1,3 @@
+export const HARD_3A = [
+['SULIT','Peraturan Jabatan PPAT','PPAT diminta membuat akta untuk dirinya sendiri. Sikap yang tepat adalah ...',['Menolak membuat akta tersebut.','Tetap membuat akta.','Meminta saksi mengganti dirinya.','Menghapus nomor akta.','Mengabaikan larangan.'],'PPAT harus netral dan tidak membuat akta ketika dirinya berkepentingan langsung.']
+];
